1439d7c37f8dd3c50fb9cca712df167ac4ed9789
baragu <baraguantonyy@gmail.com>
2024-06-04 22:43:33 +0300

    updated landing page template

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,14 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
